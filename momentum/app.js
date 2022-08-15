98e8c4c713951a20f4b70cf50c54179074d29430
Seong-Gyeong <heather128@ewhain.net>
2022-08-15 21:03:10 +0900

    JS 객체 개념 실습

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,14 @@
-alert('hi');
+const player = {
+    name : "Heather",
+    points : 10,
+    fat : false,
+    sayHello : function() {
+        console.log("Hi, I'm " + this.name);
+    }
+};
+
+console.log(player);
+console.log(player.name);
+player.lastName = "Ryu";
+console.log(player);
+player.sayHello();
