1b240b20b0195634946e15e8949f4110081e5948
SEONGGYEONG RYU <63237389+Seong-Gyeong@users.noreply.github.com>
2022-08-17 23:18:08 +0900

    localstorage에 투두 저장하고 삭제하기

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,21 +2,65 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
-//todo 추가하는 함수 
-function painTodo(newTodo) {
-   const li = document.createElement("li");
-   const span = document.createElement("span");
-   li.appendChild(span);
-   span.innerText = newTodo;
-   toDoList.appendChild(li);
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+//localStorage에 투두 저장하는 함수
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
 }
 
 
+//todo 삭제하는 함수 
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    //target : 클릭된 HTML 요소(버튼) , parentElement : target의 부모(li)
+    li.remove();
+}
+
+//todo 화면에 그리는 함수
+function paintTodo(newTodo) {
+   const li = document.createElement("li");
+   const span = document.createElement("span");
+   span.innerText = newTodo;
+   const button = document.createElement("button",deleteTodo);
+   button.addEventListener("click", deleteTodo);
+   button.innerText = "❌";
+   li.appendChild(span);
+   li.appendChild(button);
+   toDoList.appendChild(li);
+}
+
+//투두를 추가하고 저장하고 화면에 보여주는 함수
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value; //투두 적은 거 변수에 넣고
     toDoInput.value = ""; //투두 적는 칸 비우기
-    painTodo(newTodo); //변수에 넣은 todo 추가하기
+    const newTodoObject = {
+        text : newTodo,
+        id : Date.now(), //랜덤 아이디 부여
+    };
+    toDos.push(newTodoObject); //배열에 todo 추가하기 
+    paintTodo(newTodo); //변수에 넣은 todo 화면에 그려주기
+    saveTodos(); //todo 저장
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+/*
+function sayHello(item){ //item:JS는 이 함수가 실행되는 item(parsedToDos)를 제공해줌! event처럼
+    console.log("this is the turn of " + item);
+} 
+*/
+
+const savedTodos = localStorage.getItem(TODOS_KEY); //문자열
+
+if(saveTodos != null) { //saveTodos가 있다면 
+    const parsedToDos = JSON.parse(savedTodos); //배열(객체)
+    toDos = parsedToDos; //새로고침할 때마다 예전에 적은 투두들이 날아가지 않게!
+    //parsedToDos.forEach((item) => console.log("this is the turn of " + item));
+    //forEach : 배열에 있는 요소의 개수만큼 인자로 들어간 함수를 실행함 
+    parsedToDos.forEach(paintTodo); //paintTodo의 인자에 배열의 요소를 JS가 집어넣어줌 
+}
+
