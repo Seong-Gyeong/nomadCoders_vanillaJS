095fc5c88f2648664497998ee0ce4cd107775d7e
SEONGGYEONG RYU <63237389+Seong-Gyeong@users.noreply.github.com>
2022-08-19 18:55:58 +0900

    fetch 이용해 날씨, 도시 정보 화면에 출력하기

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,9 +1,19 @@
-//브라우저에서 위치 좌표를 준다. 
+const API_KEY = "2675399b00f045312fcb1aff72bc60b7";
 
-function onGeoOk() {//JS가 Success 함수에 매개변수로 GeolocationPosition Object를 준다
+//브라우저에서 위치 좌표를 준다. 
+function onGeoOk(position) {//JS가 Success 함수에 매개변수로 GeolocationPosition Object를 준다
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude ;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude ;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metirc`;
+    //units=metirc => 화씨온도를 섭씨온도로 바꿈
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText=  data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`; //날씨 / 기온 
+    }); //JS가 URL을 부름 -> seoul clouds
 }
 
 function onGeoError() {
@@ -11,3 +21,4 @@ function onGeoError() {
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
