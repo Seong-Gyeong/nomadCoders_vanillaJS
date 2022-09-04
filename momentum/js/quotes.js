a68bba6db01b92e5a727aee22939d1e052aa5594
SEONGGYEONG RYU <63237389+Seong-Gyeong@users.noreply.github.com>
2022-09-04 17:36:32 +0900

    명언 일부 수정

--- momentum/js/quotes.js
+++ momentum/js/quotes.js
@@ -1,50 +1,52 @@
 const quotes = [
-{
-    quote : "Happiness depends upon ourselves.",
-    author : "Aristotle"
-},
-{
-    quote : "The purpose of our lives is to be happy.",
-    author : "Dalai Lama"
-},
-{
-    quote : "Your time is limited, so don’t waste it living someone else’s life.",
-    author : "Steve Jobs"
-},
-{
-    quote : "You must expect great things of yourself before you can do them.",
-    author : "Michael Jordan"
-},
-{
-    quote : "It takes 20 years to build a reputation and five minutes to ruin it. If you think about that, you'll do things differently.",
-    author : "Warren Buffett"
-},
-{
-    quote : "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
-    author : "Buddha"
-},
-{
-    quote : "Happiness is a journey, not a destination.",
-    author : "Buddha"
-},
-{
-    quote : "Happiness is not something readymade. It comes from your own actions.",
-    author : "Dalai Lama"
-},
-{
-    quote : "The most important thing is to enjoy your life—to be happy—it's all that matters.",
-    author : "Audrey Hepburn"
-},
-{
-    quote : "Happiness is the secret to all beauty. There is no beauty without happiness.",
-    author : "Christian Dior"
-}
+  {
+    quote: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+  },
+  {
+    quote: "The purpose of our lives is to be happy.",
+    author: "Dalai Lama",
+  },
+  {
+    quote:
+      "Your time is limited, so don’t waste it living someone else’s life.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "You must expect great things of yourself before you can do them.",
+    author: "Michael Jordan",
+  },
+  {
+    quote: "해가 뜨기 전 새벽이 가장 어두우니까",
+    author: "Tomorrow by BTS",
+  },
+  {
+    quote: "Die with memories, not dreams",
+    author: "Anonymous",
+  },
+  {
+    quote: "Happiness is a journey, not a destination.",
+    author: "Buddha",
+  },
+  {
+    quote:
+      "Happiness is not something readymade. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    quote: "남과 비교하면 당신의 행복을 망칠 수 있다",
+    author: "꾸뻬씨의 행복여행",
+  },
+  {
+    quote: "귀는 친구를 만들고, 입은 적을 만든다",
+    author: "탈무드",
+  },
 ];
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-const todaysQuote = quotes[Math.floor(Math.random()*quotes.length)];
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
 author.innerText = todaysQuote.author;
